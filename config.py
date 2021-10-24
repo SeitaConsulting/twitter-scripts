78009623355b286fe8d3b12b12af8105618a236a
Ubuntu <ubuntu@ip-172-31-42-177.ap-northeast-1.compute.internal>
2021-10-25 00:58:47 +0900

    first commit

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-key_api = 'xFQLXKI2ieMJqcAI1MamymRdq'
-key_apisecret = 'INMlb9SczJ0nP1cTl4NORa09UChc8zrWl6EOwkb9n8mj4ggFNL'
-key_token = '1340727282-8Mwc83A5PoZ4hm6eSoFZLigvZ0kyo7DF5FwCVmm'
-key_tokensecret = '0ekcZBoUkxce2wk3VNJaxvKKawRGYbcW5g7x7VUqzhZKt'
+key_api = ''
+key_apisecret = ''
+key_token = ''
+key_tokensecret = ''
 
 CONSUMER_KEY = key_api
 CONSUMER_SECRET = key_apisecret
